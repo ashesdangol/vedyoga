e64a66336ec7be2dd439d80253dd868fce4c81af
unknown <ashesdangol@hotmail.com>
2020-06-05 00:19:24 +1000

    managed timer for search field

--- app/public/wp-content/themes/vedyoga/js/custom.js
+++ app/public/wp-content/themes/vedyoga/js/custom.js
@@ -36,8 +36,10 @@ class Search {
     this.openButton = $('.js-search-trigger');
     this.closeButton =$('.search-overlay__close');
     this.searchOverlay = $('.search-overlay');
+    this.searchField = $('#searchbox__input');
     this.events();
     this.isOverlayOpen = false;
+    this.typingTimer;
   }
   // 2 Events
   events(){
@@ -45,8 +47,15 @@ class Search {
     this.closeButton.on('click',this.closeOverlay.bind(this));
     // $(document).on("keyup", this.keyPressDispatcher.bind(this));
     $(document).on("keydown", this.keyPressDispatcher.bind(this));
+    this.searchField.on("keydown", this.typingLogic.bind(this));
   }
   // 3 Methods (functions , actions etc)
+
+  typingLogic(){
+    clearTimeout(this.typingTimer);
+    this.typingTimer = setTimeout(function(){console.log('this is a time out test')}, 2000);
+  }
+
   keyPressDispatcher(e){
     // console.log(e.keyCode);
     if (e.keyCode == 83 && !this.isOverlayOpen) {
